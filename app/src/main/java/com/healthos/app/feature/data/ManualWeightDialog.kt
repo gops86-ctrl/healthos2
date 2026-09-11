08614acfa0b0e89c19770b6bc4f078d0d36283f6
gops86-ctrl <adarshhari86@gmail.com>
2026-09-11 16:10:49 +0530

    Opt in to Material3 date picker API

--- app/src/main/java/com/healthos/app/feature/data/ManualWeightDialog.kt
+++ app/src/main/java/com/healthos/app/feature/data/ManualWeightDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -22,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import java.text.DateFormat
 import java.util.Date
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ManualWeightDialog(
     viewModel: DataViewModel,
